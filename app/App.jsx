a608cb38dd4c5b4a33469d9ef0b7fc839820d160
Radek Los <radek.los@gmail.com>
2016-10-09 20:19:11 +0200

    Simple template

--- app/App.jsx
+++ app/App.jsx
@@ -2,18 +2,36 @@ import React from 'react';
 import {Link} from 'react-router'
 
 import NavLink from './modules/NavLink'
+import {Navbar, NavItem, MenuItem, Nav, NavDropdown} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const navbarInstance = (
+    <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a className="navbar-brand" href="index.html">HLD.</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav pullRight>
+                <LinkContainer to="/repos"><NavItem>Repos</NavItem></LinkContainer>
+                <LinkContainer to="/about"><NavItem>About</NavItem></LinkContainer>
+                <LinkContainer to="/join"><NavItem>Join</NavItem></LinkContainer>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+);
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>React Router Tutorial</h1>
-                <ul role="nav">
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/repos">Repos</NavLink></li>
-                </ul>
+                {navbarInstance}
 
-                {this.props.children}
+                <div className="container">
+                    {this.props.children}
+                </div>
             </div>
       );
     }
