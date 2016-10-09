357bd7eea33c03e70f0beac102ed73bc43df1063
Radek Los <radek.los@gmail.com>
2016-10-09 17:01:09 +0200

    Bootstrap

--- app/App.jsx
+++ app/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router'
+
+import NavLink from './modules/NavLink'
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello HLD!!!
-         </div>
+    render() {
+        return (
+            <div>
+                <h1>React Router Tutorial</h1>
+                <ul role="nav">
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/repos">Repos</NavLink></li>
+                </ul>
+
+                {this.props.children}
+            </div>
       );
-   }
+    }
 }
 
 export default App;
--- main.js
+++ main.js
@@ -2,4 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import { Router, Route, hashHistory } from 'react-router'
+
+import About from './app/modules/About'
+import Repos from './app/modules/Repos'
+
+
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        <Route path="/repos" component={Repos}/>
+        <Route path="/about" component={About}/>
+        </Route>
+    </Router>),
+    document.getElementById('app')
+);
